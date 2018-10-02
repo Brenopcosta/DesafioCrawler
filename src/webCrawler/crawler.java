04d922c4b732584ffd30d37b280e1276ceca2802
Breno Costa <breno.costa182@gmail.com>
2018-10-02 02:25:28 -0300

    Adicionando lógica para pegar data dentro de uma notícia

--- src/webCrawler/crawler.java
+++ src/webCrawler/crawler.java
@@ -1,6 +1,7 @@
 package webCrawler;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,8 +14,10 @@ public class crawler{
 		System.setProperty("webdriver.chrome.driver" , localExe);		
 		WebDriver driver = new ChromeDriver();
 		
-		
 		driver.get("http://g1.globo.com/ultimas-noticias.html");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		driver.manage().window().maximize();
+		js.executeScript("window.scrollBy(0,1000)");
 		
 		java.util.List<WebElement> listaDeElementosDeNoticia = driver.findElements(By.className("feed-post-body"));;
 		
@@ -27,8 +30,25 @@ public class crawler{
 			
 			String texto = elemento.findElement(By.className("feed-post-body-resumo")).findElement(By.className("_o")).getText();
 			System.out.println(texto+"\n");
-			System.out.println("\n \n \n \n" );
 			
+			
+			WebDriver noticia = new ChromeDriver();
+			noticia.get(url);
+			try {
+			String data = noticia.findElement(By.tagName("time")).getText();
+			System.out.println(data);
+			}
+			catch(Exception e) {
+				String data = noticia.findElement(By.className("playlist__details__datetime")).getText();
+				System.out.println(data);
+				System.out.println("\n \n \n \n" );
+				noticia.close();
+				continue;
+			}
+			
+			noticia.close();
+			
+			System.out.println("\n \n \n \n" );
 			
 				
 		}
