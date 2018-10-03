df441de4239622aff5dac63343bdc94d1cea83ca
Breno Costa <breno.costa182@gmail.com>
2018-10-03 10:56:00 -0300

    Retirando a lógica para tratar vídeos e playlist

--- src/webCrawler/FuncoesCrawler.java
+++ src/webCrawler/FuncoesCrawler.java
@@ -15,82 +15,59 @@ public class FuncoesCrawler {
 	private String texto;
 	private String data;
 	private int contadorDeNoticias = 0;
+
 	public void executarCrawler() {
-		System.setProperty("webdriver.chrome.driver" , localExe);		
+		System.setProperty("webdriver.chrome.driver", localExe);
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("http://g1.globo.com/ultimas-noticias.html");
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		driver.manage().window().maximize();
-		js.executeScript("window.scrollBy(0,5000)");
-		
-		java.util.List<WebElement> listaDeElementosDeNoticia = driver.findElements(By.className("feed-post-body"));;
-		
+		js.executeScript("window.scrollBy(0,10000)");
+
+		java.util.List<WebElement> listaDeElementosDeNoticia = driver.findElements(By.className("feed-post-body"));
+		;
+
 		for (WebElement elemento : listaDeElementosDeNoticia) {
-			if(contadorDeNoticias == 10) {
+			if (contadorDeNoticias == 10) {
 				break;
 			}
-						
+
 			titulo = elemento.findElement(By.className("feed-post-link")).getText();
 			url = elemento.findElement(By.className("feed-post-link")).getAttribute("href");
-					
+
 			WebDriver noticia = new ChromeDriver();
 			noticia.get(url);
-			
+
 			try {
-			data = noticia.findElement(By.tagName("time")).getText();
+				data = noticia.findElement(By.tagName("time")).getText();
+			} catch (Exception e) {
+				noticia.close();
+				continue;
 			}
-			catch(Exception e) {
-				try{
-					data = noticia.findElement(By.className("playlist__details__datetime")).getText();
-				}
-				catch (Exception x) {
-					try {
-						data= noticia.findElement(By.className("live-at")).getText();
-					}catch (Exception z) {
-						noticia.close();
-						continue;					
-					}
-				}
-			}
-			
-			
+
 			try {
-			List<WebElement> listaDeElementosComTexto = noticia.findElements(By.className("content-text__container"));
-			texto = listaDeElementosComTexto.get(0).getText();
-			
-			for(int i = 1 ; i < listaDeElementosComTexto.size();i++) {
-				texto = texto +"\n"+listaDeElementosComTexto.get(i).getText(); 
-				}
-			}catch (Exception e) {
-				try {
-				List<WebElement> listaDeElementosComTexto = noticia.findElements(By.className("cb-summary__item"));
+				List<WebElement> listaDeElementosComTexto = noticia
+						.findElements(By.className("content-text__container"));
 				texto = listaDeElementosComTexto.get(0).getText();
-				
-				for(int i = 1 ; i < listaDeElementosComTexto.size();i++) {
-					texto = texto +"\n"+listaDeElementosComTexto.get(i).getText(); 
-					}	
-				}catch (Exception k ) {
-					System.out.println(titulo + "\n");
-					System.out.println(url + "\n");
-					System.out.println(texto + "\n");
-					System.out.println(data + "\n \n \n");
-					noticia.close();
-					continue;
+
+				for (int i = 1; i < listaDeElementosComTexto.size(); i++) {
+					texto = texto + "\n" + listaDeElementosComTexto.get(i).getText();
 				}
-				
+			} catch (Exception e) {
+				noticia.close();
+				continue;
 			}
-			
+
 			System.out.println(titulo + "\n");
 			System.out.println(url + "\n");
 			System.out.println(texto + "\n");
 			System.out.println(data + "\n \n \n");
+
 			noticia.close();
 			contadorDeNoticias++;
-		}		
+		}
 		driver.close();
 	}
 
-		
-	}
-
+}
